7996ecf88e87d7e971cf88ccdc8fd94e66c6f84d
Light-of-the-World <bestwickwade@gmail.com>
2026-01-02 19:29:42 -0600

    Metadata updates

--- QuestsExtended/Plugin.cs
+++ QuestsExtended/Plugin.cs
@@ -15,7 +15,7 @@ using static QuestsExtended.Patches.QEFromTraderScreensGroupPatch;
 
 namespace QuestsExtended;
 
-[BepInPlugin("com.dirtbikercjandlotw.QuestsExtended", "Quests Extended", "4.0.1")]
+[BepInPlugin("com.dirtbikercjandlotw.QuestsExtended", "Quests Extended", "4.0.2")]
 public class Plugin : BaseUnityPlugin
 {
     internal const int TarkovVersion = 40087;
